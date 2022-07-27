69336feb02f4c10cb8b43edcc9a888289e30cb71
niru <nirusanant97@gmail.com>
2022-07-27 16:49:01 +0530

    product table migration

---
+++ database/migrations/2022_07_27_105636_create_products_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->string('ProductName')->primary();
+            $table->string('CategoryName')->foreign('CategoryName')->references('Name')->on('categories');
+            $table->float('UnitPrice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
